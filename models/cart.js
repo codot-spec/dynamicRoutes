8a08ddf0b8d1555ce800fafa6258b947fd84869b
codot-spec <exform.arc@gmail.com>
2024-12-01 16:31:32 -0800

    Edit/delete

--- models/cart.js
+++ models/cart.js
@@ -1,5 +1,6 @@
 const fs=require('fs');
 const path=require('path');
+const { getProducts } = require('../controllers/admin');
 
 const p = path.join(
     path.dirname(process.mainModule.filename),
@@ -18,7 +19,7 @@ module.exports=class Cart{
    cart=JSON.parse(fileContent);
         }
     //Analyze the cart=>find existing product    
-    const existingProductIndex=cart.products.find(prod=>prod.id===id);
+    const existingProductIndex=cart.products.findIndex(prod=>prod.id===id);
     const existingProduct=cart.products[existingProductIndex]
     let updatedProduct;
      //Add new product/updating quantity
@@ -38,4 +39,39 @@ module.exports=class Cart{
     });
     });
    }
+
+   static deleteProduct(id,productPrice)
+   {
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            return;
+        }
+        const updatedCart={...JSON.parse(fileContent)};
+        const product=updatedCart.products.find(prod=>prod.id===id);
+        if(!product){
+            return;
+        }
+        const productQty=product.qty;
+        updatedCart.products =  updatedCart.products.filter(prod => prod.id!==id);
+        updatedCart.totalPrice=updatedCart.totalPrice - productPrice * productQty;
+
+        fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+            console.log(err);
+        });
+    })
+   }
+
+
+static getCart(cb)
+{
+    fs.readFile(p,(err,fileContent) => {
+       const cart=JSON.parse(fileContent);
+       if(err){
+        cb(null);
+       } 
+       else{
+        cb(cart);
+       }
+    })
 }
+};
