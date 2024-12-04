74779b65cc2ac2b08c37a5d4c6671a367064785c
codot-spec <exform.arc@gmail.com>
2024-12-03 19:11:04 -0800

    CartManytoMany

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
-const Product=require('./models/product');
-const User=require('./models/user')
+const Product = require('./models/product');
+const User = require('./models/user')
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -40,6 +42,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {contraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
 .sync()
@@ -55,11 +61,12 @@ sequelize
     return user;
 })
 .then(user =>{
-    console.log(user);
+    return user.createCart();
+})
+.then(cart =>{
     app.listen(3000);
 })
 .catch(err => {
     console.log(err)
 });
-
 
